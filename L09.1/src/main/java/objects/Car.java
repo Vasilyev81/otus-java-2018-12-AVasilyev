749a88dba6d49bc9bb4e0388904456f5fe72f87b
Vasilyev81 <vasilyevandrey152@gmail.com>
2019-04-29 14:10:06 +0300

    some work in L09

---
+++ L09.1/src/main/java/objects/Car.java
@@ -0,0 +1,17 @@
+package objects;
+
+public class Car {
+	private final String color;
+	private int doors;
+	private int drive;
+	private final int maxSpeed;
+	private final double engineVolume;
+
+	public Car() {
+		this.color = "black";
+		this.doors = 4;
+		this.drive = 4;
+		this.maxSpeed = 165;
+		this.engineVolume = 2.7;
+	}
+}
--- L09.1/src/main/java/otj/JsonSimpleExecutor.java
+++ L09.1/src/main/java/otj/JsonSimpleExecutor.java
@@ -1,4 +1,9 @@
 package otj;
 
+import org.json.simple.JSONObject;
+
 public class JsonSimpleExecutor {
+	public JSONObject putObject(){
+		return new JSONObject();
+	}
 }
